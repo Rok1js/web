eb5090af2e34c922a8b6afc7c1e04289f18e4c89
Rok1js <cspfflv@gmail.com>
2021-03-08 18:32:51 +0200

    Laukums 2.1

--- js/laukums.js
+++ js/laukums.js
@@ -3,20 +3,17 @@ var col = [];
 var vertibas = [];
 var vartibasn = 0;
 var laukums;
-const minesana = ["2+2","3*3","2+2*2","4^2","90/9","5x+6=56","6x^2-6x+5","30-21","4*17",""];
-const minesanaatbildes = ["4","9"];
-var minesanashufled = ["2+2","4","3*3","9"];
 var x;
 var problemList = [];
 var problemListAnswers = [];
 var problemListShuffe = [];
-
+var meginajumi = 0;
 
 
 function checkmyinput () {
     // Get inputfield
     var inputfield = document.getElementById("x");
-    
+
 
     // Get value from inputfield
     var inputval = inputfield.value;
@@ -48,6 +45,10 @@ function generateProblem() {
 document.getElementById("poga").onclick = function(){
   problemList.length = 0;
   problemListAnswers.length = 0;
+  pari = 0;
+  pirmaisklikskis = 0;
+  laiks = 0;
+  meginajumi = 0;
    x = parseInt(document.getElementById("x").value); // rindas
   if (r != 0) {
     document.getElementById("row").remove();
@@ -65,16 +66,6 @@ document.getElementById("poga").onclick = function(){
     n++;
   }
   shuffle(t);
-
-  for (var i = 0; i < x*x; i++) { // even values for i
-
-    vertibas[i] = vartibasn++;
-
-}
-
-
-
-
 
 while (problemList.length < ((x*x)/2)) {
   var math_it_up = {
@@ -100,7 +91,6 @@ for (var i = 0; i < problemList.length+problemListAnswers.length; i++) { // even
 }
 
 console.log(problemListShuffe);
-shuffle(vertibas);
 shuffle(problemListShuffe);
 console.log(vertibas);
 
@@ -110,7 +100,7 @@ console.log(vertibas);
   var row = document.createElement("DIV");
   row.setAttribute("id","row");
   laukums.appendChild(row);
-  row.style.width = `${50*x}px`;
+  row.style.width = `${60*x + x*10}px`;
   row.style.margin = `0 auto`;
   row.style.padding = `0 !important`;
   row.style.fontSize =  `0px`;
@@ -148,6 +138,30 @@ function shuffle(array) {
   }
   return array;
 }
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+
 var o = 0;
 var klikski = 0;
 var pag = [];
@@ -155,7 +169,21 @@ var pag2 = [];
 var numurs = 0;
 var irVien = false;
 var irDivi = false;
+var pari = 0;
+var pirmaisklikskis = 0;
+var laiks = 0;
+var sekunde;
+
+function myTimer() {
+  laiks++;
+  console.log("sekunde");
+}
+
 function flip(divs) {
+  pirmaisklikskis++;
+  if (pirmaisklikskis == 1) {
+      sekunde = setInterval(myTimer, 1000);
+  }
 if (klikski < 2) {
   pag[klikski] = divs;
   pag[klikski].style.pointerEvents = "none";
@@ -167,32 +195,48 @@ if (klikski < 2) {
     vienadi(pag);
     irDivi = true;
     laukums.style.pointerEvents = "none";
+    meginajumi++;
    if(irVien){
-      pag[0].style.background="green";
+      pag[0].style.background="#00A170";
+      pag[0].style.color= "white";
       pag[0].setAttribute("onclick","");
-      pag[1].style.background="green";
+      pag[1].style.background="#00A170";
+      pag[1].style.color= "white";
       pag[1].setAttribute("onclick","");
       laukums.style.pointerEvents = "auto";
       irDivi = false;
       klikski = 0;
+      pari++;
+      if (pari == ((x*x)/2)) {
+        modal.style.display = "block";
+        clearInterval(sekunde);
+        document.getElementById("laiks").innerHTML = `Tev tas prasīja: ${laiks} sekundes un ${meginajumi} mēģinājumus`
+        console.log("Kopējais laiks: "+ laiks+ " sekundes");
+      }
     }
   } else {
 
     if (klikski == 1) {
-      pag[0].style.background="#000";
+      pag[0].style.background="#0072B5";
+      pag[0].style.color="white";
     } else if (klikski == 2) {
-      pag[0].style.background="#000";
-      pag[1].style.background="#000";
+      meginajumi++;
+      pag[0].style.background="#0072B5";
+      pag[0].style.color="white";
+      pag[1].style.background="#0072B5";
+      pag[1].style.color= "white";
       laukums.style.pointerEvents = "none";
       console.log("heiheijio");
       setTimeout(function(){
         klikski = 0;
-        pag[0].style.background="#ffffff";
-        pag[1].style.background="#ffffff";
+        pag[0].style.background="#D2386C";
+        pag[0].style.color="transparent";
+        pag[1].style.background="#D2386C";
+        pag[1].style.color="transparent";
         pag[0].style.pointerEvents = "auto";
         pag[1].style.pointerEvents = "auto";
         laukums.style.pointerEvents = "auto";
-      }, 3000);
+      }, 1500);
     }
   }
 
